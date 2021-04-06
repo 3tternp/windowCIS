f23f1f98caa1335f16e91b74d0bf51515d9a728c
astra <punkx.0gravity@gmail.com>
2021-04-06 23:45:07 +0545

    Update Windows10.ps1

--- window10/Windows10.ps1
+++ window10/Windows10.ps1
@@ -8,7 +8,7 @@
 #https://twitter.com/SneakyWafWaf
 #Script WIN10 Version
 
-#Copyright (c) [2019] [Sneakysecdoggo]
+#Copyright (c) [2021] [Sneakysecdoggo]
 
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
